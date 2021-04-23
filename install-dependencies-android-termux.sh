7ac18e3fde5e46baa08b13b7532698302c4777ce
Sy Le <lenguyensy@gmail.com>
2021-04-23 16:32:17 -0700

    Update install-dependencies-android-termux.sh

--- install-dependencies-android-termux.sh
+++ install-dependencies-android-termux.sh
@@ -3,3 +3,14 @@ pkg install fzf
 pkg install vim
 pkg install git
 pkg install python
+
+echo '''
+# Send the Escape key.
+back-key=escape
+
+# black theme
+use-black-ui = true
+''' > ~/.termux/termux.properties
+
+# reload termux
+termux-reload-settings
