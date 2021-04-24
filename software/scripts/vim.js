906016f94f85d1ebd3d80c57111c788f1ff93a58
Sy Le <lenguyensy@gmail.com>
2021-04-23 18:47:44 -0700

    hide line number by default

--- software/scripts/vim.js
+++ software/scripts/vim.js
@@ -71,7 +71,7 @@ async function doWork() {
     set hlsearch    " highlight all search results
     set ignorecase  " do case insensitive search
     set noswapfile  " disable swap file
-    set number      " display line number
+    "set number      " display line number
     set showmatch
 
 
