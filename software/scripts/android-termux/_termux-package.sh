88ea5431675cd769ee91da13aa56ac0a6e28138e
Sy Le <lenguyensy@gmail.com>
2021-04-24 09:21:17 -0700

    added more log lines

--- software/scripts/android-termux/_termux-package.sh
+++ software/scripts/android-termux/_termux-package.sh
@@ -1,9 +1,11 @@
 # termux packages
 function packageInstall(){
-  echo "  >> pkg install $@"
+  echo "    >> pkg install $@"
   pkg install -y "$@" &> /dev/null
 }
 
+echo '  >> Install packages'
+packageInstall proot # needed for android termux fhd fixes
 packageInstall nodejs
 packageInstall fzf
 packageInstall vim
@@ -15,15 +17,17 @@ packageInstall perl
 packageInstall jq 
 
 # clean loose packages
+echo '  >> Clean up loose metadata for pkg'
 pkg clean  &> /dev/null
 pkg autoclean  &> /dev/null
 
 # create the termux prop dir
+echo '  >> Initiating termux properties'
 mkdir -p ~/.termux
 
 
 # change root
 # note that android termux does not have /tmp mapped correctly - this allow us to do things with git clone 
 # https://wiki.termux.com/wiki/Differences_from_Linux
-packageInstall proot 
-termux-chroot
+termux-chroot &> /dev/null
+
