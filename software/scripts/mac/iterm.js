6b9da4de93f95818d9d6bb6131924523837d0721
Sy Le <lenguyensy@gmail.com>
2021-04-25 09:52:32 -0700

    added keybindings for iterm

--- software/scripts/mac/iterm.js
+++ software/scripts/mac/iterm.js
@@ -6,15 +6,44 @@ async function doWork() {
     process.exit();
   }
 
-  const targetPath = path.join(
-    BASE_SY_CUSTOM_TWEAKS_DIR,
-    "mac",
-    "iterm.Dracula.itermcolors"
-  );
+  const targetPath = path.join(BASE_SY_CUSTOM_TWEAKS_DIR, "mac");
 
   console.log("    >> Iterm Dracula Theme Path", targetPath);
 
-  const url =
-    "https://raw.githubusercontent.com/synle/ubuntu-setup/master/themes/mac/iterm.Dracula.itermcolors";
-  await downloadFile(url, targetPath);
+  await downloadFile(
+    "https://raw.githubusercontent.com/synle/ubuntu-setup/master/themes/mac/iterm.Dracula.itermcolors",
+    path.join(targetPath, "iterm.Dracula.itermcolors")
+  );
+
+  console.log("    >> Iterm Keymap", targetPath);
+  writeText(
+    path.join(targetPath, "iterm.itermkeymap"),
+    `
+    {
+      "Key Mappings": {
+        "0xf703-0x300000-0x7c": {
+          "Label": "",
+          "Action": 19,
+          "Text": ""
+        },
+        "0xf701-0x300000-0x7d": {
+          "Label": "",
+          "Action": 21,
+          "Text": ""
+        },
+        "0xf700-0x300000-0x7e": {
+          "Label": "",
+          "Action": 20,
+          "Text": ""
+        },
+        "0xf702-0x300000-0x7b": {
+          "Label": "",
+          "Action": 18,
+          "Text": ""
+        }
+      },
+      "Touch Bar Items": {}
+    }
+  `.trim()
+  );
 }
