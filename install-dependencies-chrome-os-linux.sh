f180c8c44243e7719e2322acbbdb791ccc0f9cdd
Sy Le <lenguyensy@gmail.com>
2021-07-26 15:16:23 -0700

    Update install-dependencies-chrome-os-linux.sh

--- install-dependencies-chrome-os-linux.sh
+++ install-dependencies-chrome-os-linux.sh
@@ -1,11 +1,8 @@
-sudo apt-get update
-sudo apt-get install -y curl git vim python
+sudo apt-get update -y
 
-# set up as lightweight
-. /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/synle/bashrc/master/setup-lightweight.sh?$(date +%s))"
-
-# setting up sublime
-export TEST_SCRIPT_FILES="""
-software/scripts/sublime-text-configurations.js
-software/scripts/sublime-text-keybindings.js
-""" && curl -s https://raw.githubusercontent.com/synle/bashrc/master/test-live.sh | bash
+sudo apt-get install -y \
+  curl \
+  git \
+  vim \
+  python \
+&& echo '> Done Installing Dependencies...'
