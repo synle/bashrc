8cdc9eb746559e1decacd05f9b6226a86d655275
Sy Le <lenguyensy@gmail.com>
2021-07-26 15:17:19 -0700

    Update setup-dependencies.sh

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -19,13 +19,15 @@ echo '>> started installing dependencies...'
 if [ $is_os_darwin_mac == "1" ]; then
   echo '>> started install-dependencies-mac.sh'
   curl -s https://raw.githubusercontent.com/synle/bashrc/master/install-dependencies-mac.sh | bash -
+elif [ $is_os_chromeos == "1" ]; then
+  echo '>> started install-dependencies-chrome-os-linux.sh'
+  curl -s https://raw.githubusercontent.com/synle/bashrc/master/install-dependencies-chrome-os-linux.sh | bash -
 elif [ $is_os_ubuntu == "1" ]; then
   echo '>> started install-dependencies-ubuntu.sh'
   curl -s https://raw.githubusercontent.com/synle/bashrc/master/install-dependencies-ubuntu.sh | bash -
 else
   echo '>> skipped - This OS is not supported'
 fi
-
 
 # if windows (note that we separate it from the Ubuntu block for WSL)
 if [ $is_os_window == "1" ]
