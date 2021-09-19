bb5f280562c313c096b175720cd5d22c079bff89
Sy Le <lenguyensy@gmail.com>
2021-09-19 10:00:22 -0700

    added build (#1)
    * added build
    
    * Server Automated Build
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- package.sh
+++ package.sh
@@ -1,6 +1,8 @@
 # This script will compile all the common configs
 # used for sublime and vscode keybindings
 
+mkdir -p .build
+
 export DEBUG_WRITE_TO_DIR="./.build" && \
 sh test.sh """
 software/scripts/sublime-text-keybindings.js
