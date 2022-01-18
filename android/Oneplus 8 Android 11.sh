0217242d9872316e2d24229157d7e580599e91c8
Sy Le <lenguyensy@gmail.com>
2022-01-17 17:40:26 -0800

    added debloat for oneplus 8

---
+++ android/Oneplus 8 Android 11.sh
@@ -0,0 +1,43 @@
+curl -s https://raw.githubusercontent.com/synle/bashrc/master/android/core-android.sh | sh
+#
+pm uninstall -k --user 0 com.facebook.appmanager
+pm uninstall -k --user 0 com.facebook.services
+pm uninstall -k --user 0 com.facebook.system
+pm uninstall -k --user 0 com.google.android.apps.googleassistant
+pm uninstall -k --user 0 com.google.android.apps.magazines
+pm uninstall -k --user 0 com.google.android.apps.maps # Google Maps
+pm uninstall -k --user 0 com.google.android.apps.nbu.files
+pm uninstall -k --user 0 com.google.android.apps.photos # Google Photos
+pm uninstall -k --user 0 com.google.android.apps.podcasts
+pm uninstall -k --user 0 com.google.android.apps.subscriptions.red
+pm uninstall -k --user 0 com.google.android.apps.tachyon # Google Duo
+pm uninstall -k --user 0 com.google.android.apps.walletnfcrel # Google Wallet
+pm uninstall -k --user 0 com.google.android.apps.wellbeing # Digital Wellbeing
+pm uninstall -k --user 0 com.google.android.apps.youtube.music
+pm uninstall -k --user 0 com.google.android.calendar # Gooogle Calendar
+pm uninstall -k --user 0 com.google.android.documentsui # Files 9
+pm uninstall -k --user 0 com.google.android.feedback
+pm uninstall -k --user 0 com.google.android.gm # Gmail
+pm uninstall -k --user 0 com.google.android.googlequicksearchbox # Search Widget
+pm uninstall -k --user 0 com.google.android.marvin.talkback # Talkback
+pm uninstall -k --user 0 com.google.android.music # Google Play Music
+pm uninstall -k --user 0 com.google.android.onetimeinitializer
+pm uninstall -k --user 0 com.google.android.printservice.recommendation # Mobile printing service
+pm uninstall -k --user 0 com.google.android.projection.gearhead
+pm uninstall -k --user 0 com.google.android.videos # Google Play Movies & TV
+pm uninstall -k --user 0 com.google.android.youtube # YouTube
+pm uninstall -k --user 0 com.google.ar.core # AR Core
+pm uninstall -k --user 0 com.heytap.cloud
+pm uninstall -k --user 0 com.heytap.mcs
+pm uninstall -k --user 0 com.heytap.openid
+pm uninstall -k --user 0 com.instagram.android
+pm uninstall -k --user 0 com.netflix.mediaclient
+pm uninstall -k --user 0 com.netflix.partner.activation
+pm uninstall -k --user 0 com.oneplus.backuprestore
+pm uninstall -k --user 0 com.oneplus.calculator
+pm uninstall -k --user 0 com.oneplus.contacts # Oneplus Contact
+pm uninstall -k --user 0 com.oneplus.deskclock
+pm uninstall -k --user 0 com.oneplus.note
+pm uninstall -k --user 0 com.tmobile.m1
+pm uninstall -k --user 0 com.tmobile.pr.mytmobile # my tmobile
+pm uninstall -k --user 0 net.oneplus.forums
