cd12708afda3c7f758e299f2eeae893872dbed4b
Sy Le <lenguyensy@gmail.com>
2022-05-15 09:11:21 -0700

    Cleaned up more packages for the android tv
    Update Hisense Android TV.sh
    
    Update Hisense Android TV.sh
    
    Update Hisense Android TV.sh
    
    Update Hisense Android TV.sh
    
    Update Hisense Android TV.sh

--- android/Hisense Android TV.sh
+++ android/Hisense Android TV.sh
@@ -1,11 +1,14 @@
 function removeApp(){
   echo "> Remove: " $@
-  removeApp $@ || pm uninstall -k --user 10 $@
+  pm uninstall -k --user 0 $@ || pm uninstall -k --user 10 $@
 }
 
+# Common Android TV
 removeApp air.com.vudu.air.DownloaderTablet
 removeApp android.autoinstalls.config.sony.bravia
 removeApp com.amazon.aiv.eu
+removeApp com.android.cts.ctsshim
+removeApp com.android.cts.priv.ctsshim
 removeApp com.android.htmlviewer
 removeApp com.android.printspooler
 removeApp com.android.providers.calendar
@@ -16,7 +19,8 @@ removeApp com.google.android.apps.tv.dreamx
 removeApp com.google.android.feedback
 removeApp com.google.android.inputmethod.japanese
 removeApp com.google.android.katniss
-removeApp com.google.android.leanbacklauncher
+removeApp com.google.android.leanbacklauncher # be careful with this (TV Launcher) - do this after button mapper and launcher
+removeApp com.google.android.leanbacklauncher.recommendations # be careful with this (TV Launcher) - do this after button mapper and launcher
 removeApp com.google.android.marvin.talkback
 removeApp com.google.android.music
 removeApp com.google.android.musicSuccess
@@ -35,8 +39,6 @@ removeApp com.google.android.youtube.tvkids
 removeApp com.google.android.youtube.tvmusic
 removeApp com.google.android.youtube.tvmusicSuccess
 removeApp com.google.android.youtube.tvSuccess
-removeApp com.hisense.favkey
-removeApp com.hisense.tv.protector
 removeApp com.jamdeo.tv.mediacenter
 removeApp com.opera.sdk.example
 removeApp com.pandora.android.atv
@@ -45,7 +47,16 @@ removeApp com.sony.dtv.youview
 removeApp com.youview.tv.servicehost
 removeApp tv.samba.ssm
 
+# Hisense ULED 4K Premium 75U6G (xiaoyushan)
+removeApp com.hisense.favkey
+removeApp com.hisense.service.message
+removeApp com.hisense.tv.protector
+
 # cautious!
 removeApp com.mstar.android.tv.disclaimercustomization
 removeApp com.mstar.netflixobserver # can be replaced with button mapper
+# experimental!
+removeApp com.hisense.storemode
+removeApp com.hisense.alexa
+removeApp com.hisense.hitv.hicloud.account
 # removeApp com.jamdeo.tv.mediacenterapplication
