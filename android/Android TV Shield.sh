23a7ea44bc4be3644e5b4506cb00a6828ca3498b
Sy Le <lenguyensy@gmail.com>
2022-05-29 19:33:24 -0700

    Update Android TV Shield.sh

--- android/Android TV Shield.sh
+++ android/Android TV Shield.sh
@@ -1,12 +1,21 @@
 function removeApp(){
   echo "> Remove: " $@
-  pm uninstall $@ || pm uninstall -k --user 0 $@ || pm uninstall -k --user 10 $@
+  pm uninstall $@ || pm uninstall $@ || pm uninstall -k --user 0 $@ || pm uninstall -k --user 10 $@
 }
 
 removeApp air.com.vudu.air.DownloaderTablet
+removeApp air.ITVMobilePlayer
+removeApp com.amazon.music.tv
 removeApp com.google.android.play.games
+removeApp com.google.android.videos
 removeApp com.google.android.youtube.tv
 removeApp com.google.android.youtube.tvkids
 removeApp com.google.android.youtube.tvmusic
+removeApp com.imdbtv.livingroom
+removeApp com.nvidia.bbciplayer
+removeApp com.nvidia.bbciplayer.launch
+removeApp com.nvidia.bbciplayer.launchsport
+removeApp com.nvidia.benchmarkblocker
 removeApp com.pandora.android.atv
 removeApp com.seraphic.browser
+removeApp com.zattoo.player
