a11ba98ece51a3838d528dd46e49a0d42e1b4c00
Sy Le <lenguyensy@gmail.com>
2022-08-30 07:57:19 -0700

    Update Samsung Tab S8 Ultra.sh

--- android/Samsung Tab S8 Ultra.sh
+++ android/Samsung Tab S8 Ultra.sh
@@ -1,7 +1,1 @@
-# use the one from s21 as the base and add more stuffs on top
-curl -s "https://github.com/synle/bashrc/blob/master/android/Samsung S21 Ultra.sh" | sh
-
-function removeApp(){
-  echo "> Remove: " $@
-  pm uninstall $@ || pm uninstall -k --user 0 $@ || pm uninstall -k --user 10 $@
-}
+https://github.com/synle/bashrc/blob/master/android/Samsung%20Z%20Fold%204.sh
