3d1613782f8ba362a2b0eab720cfc1f7bdeac248
Sy Le <lenguyensy@gmail.com>
2022-09-03 18:59:34 -0700

    Update registry.js

--- software/scripts/windows/registry.js
+++ software/scripts/windows/registry.js
@@ -7,23 +7,7 @@ async function doWork() {
     targetPath,
     `
 Windows Registry Editor Version 5.00
-; no lock screen
-[HKEY_LOCAL_MACHINE/SOFTWARE/Policies/Microsoft/Windows/Personalization]
-"NoLockScreen"=dword:00000001
-
-; speed up menu show delay
-[HKEY_CURRENT_USER/Control Panel/Desktop]
-"MenuShowDelay"="250"
-
-; Disable shaking to min and max windows
-[HKEY_LOCAL_MACHINE/SOFTWARE/Policies/Microsoft/Windows/Personalization]
-"DisallowShaking"=dword:00000001
-
-; Use classic menu bar for Windows 11
-[HKEY_CURRENT_USER/Software/Classes/CLSID/{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}/InprocServer32]
-@=""
-
-; Disable internet search in start menu
+; Disable internet search in start menu in windows 11
 [HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer]
 "DisableSearchBoxSuggestions"=dword:00000001
     `
