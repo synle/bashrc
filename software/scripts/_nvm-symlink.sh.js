955ea50d508ea6676f56e90a5595c88ad7f0bb76
Sy Le <lenguyensy@gmail.com>
2022-09-22 14:39:58 -0700

    Update _nvm-symlink.sh.js

--- software/scripts/_nvm-symlink.sh.js
+++ software/scripts/_nvm-symlink.sh.js
@@ -4,6 +4,7 @@ async function doWork() {
 
   console.log(echo(`    >> Symlink for nvm in global space (node, npm, npx) ${targetPath}`));
   console.log(`
+    sudo rm -rf "/usr/local/bin/node" "/usr/local/bin/npm"
     sudo ln -s "${targetPath}/bin/node" "/usr/local/bin/node" &>/dev/null
     sudo ln -s "${targetPath}/bin/npm" "/usr/local/bin/npm" &>/dev/null
   `);
