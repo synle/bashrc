468d745a06ddf5ce2f7b2f70b0dcf3d708f3dfb0
Sy Le <lenguyensy@gmail.com>
2022-09-22 07:06:11 -0700

    Update install-dependencies-mac.sh

--- install-dependencies-mac.sh
+++ install-dependencies-mac.sh
@@ -10,6 +10,38 @@ function installPackage(){
 
 if [[ $is_os_darwin_mac == "1" ]]
 then
+  ecoh '>> Animation tweaks'
+  # settings to speed up
+  # https://gist.github.com/kidpixo/78b9a40ab58e026cf9a432573e27ced5
+  #1. Disable animations when opening and closing windows.
+  defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+  #2. Disable animations when opening a Quick Look window.
+  defaults write -g QLPanelAnimationDuration -float 0
+
+  #3. Accelerated playback when adjusting the window size (Cocoa applications).
+  defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+  #4. Disable animation when opening the Info window in Finder (cmd⌘ + i).
+  defaults write com.apple.finder DisableAllAnimations -bool true
+
+  #5. Disable animations when you open an application from the Dock.
+  defaults write com.apple.dock launchanim -bool false
+
+  #6. Make all animations faster that are used by Mission Control.
+  defaults write com.apple.dock expose-animation-duration -float 0.1
+
+  #7. Disable the delay when you hide the Dock
+  defaults write com.apple.Dock autohide-delay -float 0
+  #Mail applicatie
+
+  #8. Disable the animation when you sending and replying an e-mail
+  defaults write com.apple.mail DisableReplyAnimations -bool true
+  defaults write com.apple.mail DisableSendAnimations -bool true
+
+  #9. Disable the standard delay in rendering a Web page.
+  defaults write com.apple.Safari WebKitInitialTimedLayoutDelay 0.25
+
   echo '>> Set default shell as BASH (Catalina Mods): chsh -s /bin/bash'
   touch ~/.bashrc
   touch ~/.bash_profile
