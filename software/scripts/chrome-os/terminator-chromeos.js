1a04c4bfbe081b1f80a1b0a8286dc42dd7bc63e1
Sy Le <lenguyensy@gmail.com>
2023-02-28 20:51:03 -0800

    Update terminator-chromeos.js

--- software/scripts/chrome-os/terminator-chromeos.js
+++ software/scripts/chrome-os/terminator-chromeos.js
@@ -1,5 +1,8 @@
-async function doWork() {
-  let targetPath = path.join(BASE_HOMEDIR_LINUX, '.config/terminator/config');
+async function doWork() {  
+  let targetPath = path.join(BASE_HOMEDIR_LINUX, '.config/terminator');
+  await mkdir(targetPath)
+  
+  targetPath = path.join(targetPath, 'config');
   console.log('  >> Download Terminator Config - ChromeOS ', targetPath);
 
   const content = await fetchUrlAsString('software/scripts/chrome-os/terminator-chromeos.config');
