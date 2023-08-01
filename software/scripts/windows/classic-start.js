c938fe62cb707e5399edbf6a0b56dd7c17f2af6c
Sy Le <lenguyensy@gmail.com>
2023-07-31 17:45:15 -0700

    built classic start and added link for precompiled folder in ui

--- software/scripts/windows/classic-start.js
+++ software/scripts/windows/classic-start.js
@@ -1,8 +1,10 @@
 async function doWork() {
   console.log('  >> Installing Windows Only - Classic Start Configs');
+  const res = await fetchUrlAsString(`software/scripts/windows/classic_start.config.xml`);
+
+  writeToBuildFile([['windows-classic-start-menu.xml', res, false]]);
 
   const targetPath = path.join(BASE_SY_CUSTOM_TWEAKS_DIR, 'windows', 'classic-start.xml');
-  const res = await fetchUrlAsString(`software/scripts/windows/classic_start.config.xml`);
 
   console.log('    >> Classic Start Configs', consoleLogColor4(targetPath));
   writeText(targetPath, res);
