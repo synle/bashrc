e57f6829f413b43f8d3a2d1837cfa8927cc68c0c
Sy Le <lenguyensy@gmail.com>
2023-08-08 16:08:21 -0700

    move the file association configuration into the config file and add more commonly known types to be opened with sublime

--- software/scripts/windows/file-association.js
+++ software/scripts/windows/file-association.js
@@ -2,68 +2,11 @@ const sublimeProgramBinaryName = 'sublime';
 
 const sublimeBinaryPath = `ftype ${sublimeProgramBinaryName}="C:/Program Files/Sublime Text/sublime_text.exe" "%1"`;
 
-const extensionsToOpenWithEditor = convertTextToList(`
-  csv
-  txt
-  dat
-  db
-  log
-  mdb
-  sav
-  sql
-  xml
-  email
-  eml
-  emlx
-  msg
-  oft
-  ost
-  pst
-  vcf
-  asp
-  aspx
-  cer
-  cfm
-  cgi
-  css
-  htm
-  js
-  jsp
-  part
-  php
-  py
-  rss
-  xhtml
-  c
-  cpp
-  cs
-  h
-  java
-  sh
-  swift
-  vb
-  editorconfig
-  eslintignore
-  babelrc
-  gitignore
-  prettierrc
-  json
-  env
-  npmignore
-  nvmrc
-  md
-  yml
-  info
-  nfo
-  ts
-  scss
-  sass
-  less
-  html
-  php5
-`);
+let extensionsToOpenWithEditor = [];
 
 async function doInit() {
+  extensionsToOpenWithEditor = convertTextToList(await fetchUrlAsString('software/scripts/windows/file-association.config'));
+
   associationContent = `
 ${extensionsToOpenWithEditor.map((extension) => `assoc ${extension}=${sublimeProgramBinaryName}`).join('\n')}
 ${sublimeBinaryPath}
