3f18ad7980761f77a844b37c8af0f68abcd28b4d
Sy Le <lenguyensy@gmail.com>
2023-08-08 15:55:47 -0700

    fixed association of files in windows

--- software/scripts/windows/file-association.js
+++ software/scripts/windows/file-association.js
@@ -1,69 +1,73 @@
-const sublime_path = `ftype sublime="C:/Program Files/Sublime Text/sublime_text.exe" "%1"`;
+const sublimeProgramBinaryName = 'sublime';
+
+const sublimeBinaryPath = `ftype ${sublimeProgramBinaryName}="C:/Program Files/Sublime Text/sublime_text.exe" "%1"`;
+
+const extensionsToOpenWithEditor = convertTextToList(`
+  csv
+  txt
+  dat
+  db
+  log
+  mdb
+  sav
+  sql
+  xml
+  email
+  eml
+  emlx
+  msg
+  oft
+  ost
+  pst
+  vcf
+  asp
+  aspx
+  cer
+  cfm
+  cgi
+  css
+  htm
+  js
+  jsp
+  part
+  php
+  py
+  rss
+  xhtml
+  c
+  cpp
+  cs
+  h
+  java
+  sh
+  swift
+  vb
+  editorconfig
+  eslintignore
+  babelrc
+  gitignore
+  prettierrc
+  json
+  env
+  npmignore
+  nvmrc
+  md
+  yml
+  info
+  nfo
+  ts
+  scss
+  sass
+  less
+  html
+  php5
+`);
 
 async function doInit() {
   associationContent = `
-${sublime_path}
-assoc .csv=sublime
-assoc .txt=sublime
-assoc .dat=sublime
-assoc .db=sublime
-assoc .log=sublime
-assoc .mdb=sublime
-assoc .sav=sublime
-assoc .sql=sublime
-assoc .xml=sublime
-assoc .email=sublime
-assoc .eml=sublime
-assoc .emlx=sublime
-assoc .msg=sublime
-assoc .oft=sublime
-assoc .ost=sublime
-assoc .pst=sublime
-assoc .vcf=sublime
-assoc .asp=sublime
-assoc .aspx=sublime
-assoc .cer=sublime
-assoc .cfm=sublime
-assoc .cgi=sublime
-assoc .css=sublime
-assoc .htm=sublime
-assoc .js=sublime
-assoc .jsp=sublime
-assoc .part=sublime
-assoc .php=sublime
-assoc .py=sublime
-assoc .rss=sublime
-assoc .xhtml=sublime
-assoc .c=sublime
-assoc .cpp=sublime
-assoc .cs=sublime
-assoc .h=sublime
-assoc .java=sublime
-assoc .sh=sublime
-assoc .swift=sublime
-assoc .vb=sublime
-assoc .editorconfig=sublime
-assoc .eslintignore=sublime
-assoc .babelrc=sublime
-assoc .gitignore=sublime
-assoc .prettierrc=sublime
-assoc .json=sublime
-assoc .env=sublime
-assoc .npmignore=sublime
-assoc .nvmrc=sublime
-assoc .md=sublime
-assoc .yml=sublime
-assoc .info=sublime
-assoc .nfo=sublime
-assoc .ts=sublime
-assoc .scss=sublime
-assoc .sass=sublime
-assoc .less=sublime
-assoc .html=sublime
-assoc .php5=sublime
-  `
-    .trim()
-    .replace('/', '\\\\');
+${extensionsToOpenWithEditor.map((extension) => `assoc ${extension}=${sublimeProgramBinaryName}`).join('\n')}
+${sublimeBinaryPath}
+  `.trim();
 }
 
 async function doWork() {
