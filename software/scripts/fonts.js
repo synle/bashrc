b9f0b087c4357c00b14e71a58ce390692f216b18
Sy Le <lenguyensy@gmail.com>
2023-08-08 13:57:16 -0700

    install fonts for linux

--- software/scripts/fonts.js
+++ software/scripts/fonts.js
@@ -1,14 +1,11 @@
+const linuxFontPath = '/usr/share/fonts/truetype';
+
 async function doWork() {
   const targetFontPath = path.join(BASE_SY_CUSTOM_TWEAKS_DIR, 'fonts');
 
   await mkdir(targetFontPath);
 
   console.log('  >> Download Ligatures Fonts:', targetFontPath);
-
-  if (!is_os_window && !is_os_darwin_mac && !is_os_arch_linux) {
-    console.log(consoleLogColor1('  >> Skipped : Only Mac or Windows or Arch Linux'));
-    return process.exit();
-  }
 
   const files = await listRepoDir();
 
@@ -40,6 +37,32 @@ async function doWork() {
         resolve();
       }),
     );
+  }
+
+  // for linux
+  if (filePathExist(linuxFontPath)) {
+    console.log('  >> Downloading fonts for Linux', linuxFontPath);
+    console.log('    >> sudo fc-cache -fv');
+
+    for (const font of fonts) {
+      promises.push(
+        new Promise(async (resolve) => {
+          const destination = path.join(linuxFontPath, path.basename(font));
+
+          try {
+            const url = font;
+            const downloaded = await downloadFile(url, destination);
+            if (downloaded === true) {
+              console.log(consoleLogColor3('      >> Downloaded'), consoleLogColor4(destination));
+            }
+          } catch (err) {
+            console.log(consoleLogColor3('      >> Error Downloading'), consoleLogColor4(font));
+          }
+
+          resolve();
+        }),
+      );
+    }
   }
 
   // await
