6ccb8d438079f323ed2e97481553e678a25cf912
Sy Le <lenguyensy@gmail.com>
2023-11-16 07:40:55 -0800

    added the headers and prepare the bash content up front

--- software/scripts/bash-syle-content.js
+++ software/scripts/bash-syle-content.js
@@ -2,35 +2,46 @@ async function doWork() {
   console.log('  >> Setting up .bash_syle');
 
   let res = readText(BASE_BASH_SYLE);
-
-  const contentBaseBashScript = await fetchUrlAsString(`bash-profile-barebone.sh`);
-
-  const contentCommonAliasScript = await fetchUrlAsString(`bash-profile-more-advanced.sh`);
-
   res += `\n\n\n`;
+
+  let contentBashProfileBarebone = await fetchUrlAsString(`bash-profile-barebone.sh`);
+  let contentBashProfileAdvanced = await fetchUrlAsString(`bash-profile-more-advanced.sh`);
+
+  // add the header
+  contentBashProfileBarebone = `
+##########################################################
+## begin barebone profile
+##########################################################
+
+${contentBashProfileBarebone}
+
+##########################################################
+## end barebone profile
+##########################################################
+  `.trim();
+
+  contentBashProfileAdvanced= `
+##########################################################
+## begin advanced profile
+##########################################################
+
+${contentBashProfileAdvanced}
+
+##########################################################
+## begin advanced profile
+##########################################################
+  `.trim()
 
   // barebone script
   console.log('    >> Barebone profile');
-  res += `
-##########################################################
-## barebone profile
-##########################################################
-
-${contentBaseBashScript}
-  `.trim();
+  res += contentBashProfileBarebone.trim();
 
   // append more advanced script only for fancier OS
   console.log('    >> More advanced profile');
-  if (is_os_window || is_os_darwin_mac) {
+
+  if (is_os_window || is_os_darwin_mac || is_os_ubuntu) {
     console.log('      >> Installed only for more advanced OS');
-
-    res += `
-##########################################################
-## more advanced profile
-##########################################################
-
-${contentCommonAliasScript}
-  `.trim();
+    res += contentBashProfileAdvanced.trim();
   } else {
     console.log(consoleLogColor1('      >> Skipped : Only Mac or Windows'));
   }
