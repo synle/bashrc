093e0f46e752735815de9f7139a8dae30f91ce45
Sy Le <lenguyensy@gmail.com>
2025-01-19 14:22:59 -0800

    Update winget-install-windows.ps1

--- .build/winget-install-windows.ps1
+++ .build/winget-install-windows.ps1
@@ -8,11 +8,7 @@ winget install --id  Greenshot.Greenshot
 winget install --id  HandBrake.HandBrake
 winget install --id  Inkscape.Inkscape
 winget install --id  KDE.Krita
-# winget install --id  Microsoft.VisualStudioCode
-# winget install --id  Notepad++.Notepad++
-# winget install --id  PuTTY.PuTTY
-# winget install --id  Python.Python
-winget install --id=Python.Python.3.10
+winget install --id  Python.Python.3.10
 winget install --id  SublimeHQ.SublimeMerge
 winget install --id  SublimeHQ.SublimeText.4
 winget install --id  Ultimaker.Cura
@@ -20,15 +16,20 @@ winget install --id  Valve.Steam
 winget install --id  VideoLAN.VLC
 winget install --id  VSCodium.VSCodium
 winget install --id  winget install OpenJS.NodeJS
-# winget install --id  WinMerge.WinMerge
 winget install --id  WinSCP.WinSCP
 winget install --id  dotPDNLLC.paintdotnet
-# winget install --id  Prusa3D.PrusaSlicer
-winget install --id  Docker.DockerDesktop
-##############################
-# chitubox
-##############################
 winget install --id  Zoom.Zoom
-winget install --id  AdoptOpenJDK.OpenJDK.16
-winget install --id  TheDocumentFoundation.LibreOffice
-winget install --id  Oracle.VirtualBox
+winget install --id  PuTTY.PuTTY
+winget install --id  WinMerge.WinMerge
+
+
+#### Ignored
+# winget install --id  Microsoft.VisualStudioCode
+# winget install --id  Notepad++.Notepad++ 
+# winget install --id  Python.Python
+# winget install --id  Prusa3D.PrusaSlicer
+## # chitubox # this is the slicer for resin printer Mars
+## winget install --id  Docker.DockerDesktop
+## winget install --id  AdoptOpenJDK.OpenJDK.16
+## winget install --id  TheDocumentFoundation.LibreOffice
+## winget install --id  Oracle.VirtualBox
