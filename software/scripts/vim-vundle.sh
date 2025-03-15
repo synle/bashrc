8986455be279c16bc4df9df78b90b46f880d4e26
Sy Le <lenguyensy@gmail.com>
2025-03-15 08:35:42 -0700

    added script to install vundle

--- software/scripts/vim-vundle.sh
+++ software/scripts/vim-vundle.sh
@@ -4,7 +4,7 @@ VUNDLE_URL="https://github.com/synle/vundle.git"
 VUNDLE_DIR=~/.vim/bundle/Vundle.vim
 
 echo "  >> Installing Vundle Binary - $VUNDLE_URL"
-rm -rf  ~/.vim/bundle
+# rm -rf  ~/.vim/bundle
 mkdir -p ~/.vim/bundle
 
 git clone $VUNDLE_URL $VUNDLE_DIR &> /dev/null;
@@ -12,3 +12,5 @@ git clone $VUNDLE_URL $VUNDLE_DIR &> /dev/null;
 echo '  >> Installing Vundle Plugins:'
 echo '  :PluginInstall'
 echo '  vim +PluginInstall +qall'
+
+vim +PluginInstall +qall  &> /dev/null;
