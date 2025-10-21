ce93abc5248e3eee277055c2f75c6e3783e3d076
Sy Le <lenguyensy@gmail.com>
2025-10-20 17:22:03 -0700

    added linux mint vscode and vscodium flatpack install

--- .build/vs-code.sh
+++ .build/vs-code.sh
@@ -1,9 +1,22 @@
-# for linux mint
+# for linux with flatpak
+curl -fsSL "https://raw.githubusercontent.com/synle/bashrc/refs/heads/master/.build/vs-code-configurations" \
+-o "$HOME/.var/app/com.vscodium.codium/config/VSCodium/User/settings.json"
+
+curl -fsSL "https://raw.githubusercontent.com/synle/bashrc/refs/heads/master/.build/vs-code-keybindings-linux" \
+-o "$HOME/.var/app/com.vscodium.codium/config/VSCodium/User/keybindings.json"
+
+# for linux mint / ubuntu
 curl -fsSL "https://raw.githubusercontent.com/synle/bashrc/refs/heads/master/.build/vs-code-configurations" \
 -o "$HOME/.config/VSCodium/User/settings.json"
 
 curl -fsSL "https://raw.githubusercontent.com/synle/bashrc/refs/heads/master/.build/vs-code-keybindings-linux" \
 -o "$HOME/.config/VSCodium/User/keybindings.json"
+
+curl -fsSL "https://raw.githubusercontent.com/synle/bashrc/refs/heads/master/.build/vs-code-configurations" \
+-o "$HOME/.config/Code/User/settings.json"
+
+curl -fsSL "https://raw.githubusercontent.com/synle/bashrc/refs/heads/master/.build/vs-code-keybindings-linux" \
+-o "$HOME/.config/Code/User/keybindings.json"
 
 # for mac
 curl -fsSL "https://raw.githubusercontent.com/synle/bashrc/refs/heads/master/.build/vs-code-configurations" \
