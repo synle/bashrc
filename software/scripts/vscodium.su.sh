61365b21ead88dfb2791aec028d23c86b62ceeb9
Sy Le <lenguyensy@gmail.com>
2025-11-01 20:39:16 -0700

    added script to run codium

---
+++ software/scripts/vscodium.su.sh
@@ -0,0 +1,14 @@
+# Install VSCodium (only if running on Ubuntu and not Windows)
+if [ "${is_os_ubuntu}" -eq 1 ] && [ "${is_os_window}" -eq 0 ]; then
+  version="1.105.17075"
+  file="codium_${version}_amd64.deb"
+  url="https://github.com/VSCodium/vscodium/releases/download/${version}/${file}"
+
+  pushd /tmp >/dev/null || exit
+  echo "Installing VSCodium v${version}..."
+  wget -q "$url" -O "$file"
+  sudo dpkg -i "$file" >/dev/null 2>&1 || sudo apt -f install -y >/dev/null 2>&1
+  popd >/dev/null || exit
+
+  echo "VSCodium v${version} installed successfully."
+fi
