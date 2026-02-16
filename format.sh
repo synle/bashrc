a66b31ba4be0ba9c3793bdd3a45f53437466e369
Sy Le <lenguyensy@gmail.com>
2026-02-16 12:16:18 -0800

    streamline git config and format

--- format.sh
+++ format.sh
@@ -1,1 +1,3 @@
-curl -s https://raw.githubusercontent.com/synle/gha-workflow/refs/heads/main/format.sh | bash -
+FORMAT_SCRIPT_URL=https://raw.githubusercontent.com/synle/gha-workflow/refs/heads/main/format.sh
+echo ">> formatting script: $FORMAT_SCRIPT_URL"
+curl -s "$FORMAT_SCRIPT_URL" | bash - > /dev/null 2>&1
