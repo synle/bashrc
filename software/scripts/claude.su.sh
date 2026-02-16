717068b2e47633f38387714c3d6c0bbd2f00a368
Sy Le <lenguyensy@gmail.com>
2026-02-16 11:48:28 -0800

    streamline claude install

--- software/scripts/claude.su.sh
+++ software/scripts/claude.su.sh
@@ -1,2 +1,5 @@
 echo '>> Installing claude'
-curl -fsSL https://claude.ai/install.sh | bash
+if ! command -v claude >/dev/null 2>&1; then
+  echo '  >> Downloading and Installing'
+  curl -fsSL https://claude.ai/install.sh | bash > /dev/null 2>&1
+fi
