8d9e8e4ca4a9936eb0d07e42d03cf39a5163376c
Sy Le <lenguyensy@gmail.com>
2026-02-17 08:55:10 -0800

    use the correct gha repo

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
-FORMAT_SCRIPT_URL=https://raw.githubusercontent.com/synle/gha-workflow/refs/heads/main/format.sh
+FORMAT_SCRIPT_URL=https://raw.githubusercontent.com/synle/gha-workflows/refs/heads/main/format.sh
 echo ">> formatting script: $FORMAT_SCRIPT_URL"
 curl -s "$FORMAT_SCRIPT_URL" | bash - > /dev/null 2>&1
