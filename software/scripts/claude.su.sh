6fc360028fcadde114a0cd2f93fff049905b344e
Sy Le <lenguyensy@gmail.com>
2026-02-21 16:50:37 -0800

    minor symlink for claude update

--- software/scripts/claude.su.sh
+++ software/scripts/claude.su.sh
@@ -2,4 +2,6 @@ echo '>> Installing claude'
 if ! command -v claude >/dev/null 2>&1; then
   echo '  >> Downloading and Installing'
   curl -fsSL https://claude.ai/install.sh | bash > /dev/null 2>&1
+
+  sudo ln -sf $(which claude) /usr/local/bin/claude
 fi
