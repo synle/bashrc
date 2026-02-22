66c536da46379f872d36725dbe3ae12815d93960
Sy Le <lenguyensy@gmail.com>
2026-02-22 12:43:54 -0800

    clean up

--- test-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-echo '''
-====================================================
->> Do a test dependencies with LOCAL code
-====================================================
-'''
-
-sh run.sh --local --run-only-prescripts --pre-scripts="setup-dependencies.sh"
