7277951bb4d432e3d784f67c15c6f55d16fb517b
Sy Le <lenguyensy@gmail.com>
2026-02-24 15:47:27 -0800

    wip clean up

--- bootstrap/dependencies-windows.sh
+++ bootstrap/dependencies-windows.sh
@@ -32,8 +32,8 @@ if [ "$is_os_window" = "1" ]; then
   echo 'New-Item $profile -Type File -Force'
 
   echo '>> Setting up WSL2 binary'
-  dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-  dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+  dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart || echo "Error: dism.exe needs to be run as Administrator"
+  dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart || echo "Error: dism.exe needs to be run as Administrator"
 
   echo '  >> wsl --set-default-version 2'
 
