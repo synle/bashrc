eda310a70a793f1092031633d9e5d75372f8c94c
Sy Le <lenguyensy@gmail.com>
2026-02-23 16:09:34 -0800

    wip

--- .build/sublime-text-plugins-refresh-on-focus.py
+++
@@ -1,9 +0,0 @@
-# software/scripts/sublime-text-plugins-refresh-on-focus.py
-
-import sublime
-import sublime_plugin
-
-class RefreshOnFocusListener(sublime_plugin.EventListener):
-    def on_activated_async(self, view):
-        # This triggers when a view gains focus (window or tab)
-        view.window().run_command("refresh_folder_list")
