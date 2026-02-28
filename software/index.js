a5a4036468119124902bf749cf515345990b1576
Sy Le <lenguyensy@gmail.com>
2026-02-28 14:46:11 -0800

    fix runner

--- software/index.js
+++ software/index.js
@@ -1732,6 +1732,7 @@ function processScriptFile(file, originalFile, allRepoFiles) {
     const fetchCmd = _generateScript(file, url);
     const runner = _generatePipeOutput(file, url);
     tempFileCommand = _generateTempFileCommand(fetchCmd, tmpFile, runner, file);
+    console.log(tempFileCommand)
   } else {
     console.log(echoColor3(`  >> ${originalFile} (${file}) - does not exist `));
   }
