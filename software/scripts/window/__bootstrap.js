54ac78b07d7ff5e252abf13553173e200d54c861
Sy Le <lenguyensy@gmail.com>
2026-03-01 11:40:12 -0800

    minor >

--- software/scripts/window/__bootstrap.js
+++ software/scripts/window/__bootstrap.js
@@ -19,7 +19,7 @@ async function doWork() {
     log(">>> total", pathsToCreate.length);
 
     for (const pathToCreate of pathsToCreate) {
-      log(">>> ", pathToCreate);
+      log(">>>> ", pathToCreate);
       await mkdir(pathToCreate);
     }
   }
