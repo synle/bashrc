0eca1bd7a3b88bd8bcf888bc211cb26628fc328f
Sy Le <lenguyensy@gmail.com>
2026-03-04 19:27:58 -0800

    fixed vscode install and fzf history fixes

--- software/scripts/vscodium.su.sh
+++ software/scripts/vscodium.su.sh
@@ -6,7 +6,7 @@
 [ "$is_os_chromeos" = "1" ] && { echo ">>> Skipped : Not supported on is_os_chromeos"; exit 0; }
 
 version=$(curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest \
-    | node -e "process.stdin.on('data', d => console.log(JSON.parse(d).tag_name))")
+    | node -e "let b='';process.stdin.on('data',d=>b+=d);process.stdin.on('end',()=>console.log(JSON.parse(b).tag_name))")
 
 echo "> Installing vscodium : $version"
 
